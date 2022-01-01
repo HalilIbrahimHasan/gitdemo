a0e0f9df426dd2d9c3be0502ab2fd823b9e72b75
Ibrahim <hisetkal@gmail.com>
2021-12-31 20:51:33 -0500

    new commit on master

--- src/packageOne/ClassOne.java
+++ src/packageOne/ClassOne.java
@@ -6,6 +6,7 @@ public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is GitHub session");
+        System.out.println("This is Salesforce");
     }
 
 

de09647c3c87fdf4426aa78d2810e234fed5ab06
Ibrahim <hisetkal@gmail.com>
2021-12-31 21:14:20 -0500

    commit 5

--- src/packageOne/ClassOne.java
+++ src/packageOne/ClassOne.java
@@ -8,6 +8,7 @@ public class ClassOne {
         System.out.println("This is GitHub session");
         System.out.println("This is Salesforce");
         System.out.println("This is new update on my local master branch");
+        System.out.println("This is commit 5");
     }
 
 

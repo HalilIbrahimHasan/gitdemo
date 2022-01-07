8cf10d002fa86186cdd7feee8b1078dde8361cc4
Ibrahim <hisetkal@gmail.com>
2022-01-06 20:09:33 -0500

    updates on resolving conflicts

--- src/packageOne/ClassOne.java
+++ src/packageOne/ClassOne.java
@@ -13,7 +13,7 @@ public class ClassOne {
         System.out.println("This is new update on my local master branch");
         System.out.println("This is commit 5");
         System.out.println("Here is an update on remote master branch");
-<<<<<<< HEAD
+
         System.out.println("Here is a new commit 7 from Suheyp");
 
         System.out.println("Ibrahim settar");
